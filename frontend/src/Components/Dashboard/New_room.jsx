32ca5cadfb1bc8734776879a2d571b86d98c1ecc
Alexandre BOURCIER <alexandre.bourcier@epita.fr>
2022-07-04 14:13:19 +0200

    list of friend done

--- frontend/src/Components/Dashboard/New_room.jsx
+++ frontend/src/Components/Dashboard/New_room.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 function NewRoom(props) {
   const [friendList, setFriendList] = useState([]);
+  const [listNewRoom, setListNewRoom] = useState([]);
   useEffect(() => {
     axios
       .get("http://localhost:4000/getFriends", { withCredentials: true })
@@ -11,6 +12,22 @@ function NewRoom(props) {
         setFriendList(friends);
       });
   }, []);
+
+  const handleClickFriend = (e) => {
+    if (e.target.type !== "checkbox") e.preventDefault();
+    let obj;
+    if (e.target.className !== "room") obj = e.target.parentNode;
+    else obj = e.target;
+
+    let state;
+    if (e.target.type !== "checkbox") {
+      let checkbox = obj.childNodes[0];
+      if (!checkbox.checked) checkbox.checked = true;
+      else checkbox.checked = false;
+      state = checkbox.checked;
+    } else state = e.target.checked;
+    if (state) console.log(state);
+  };
 
   return (
     <div className="Dialog_box">
@@ -27,8 +44,8 @@ function NewRoom(props) {
         <ul>
           {friendList.map((e) => {
             return (
-              <li className="room" key={e._id}>
-                <input id="check" type="checkbox"></input>
+              <li className="room" key={e._id} onClick={handleClickFriend}>
+                <input type="checkbox"></input>
                 <img src="/room_image.png" alt="user_image" />
                 <span>{e.username}</span>
               </li>
