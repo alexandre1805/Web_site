418fee2ea30bc566bee12f2d1be7243b6f09231c
alexandre1805 <alexandre.bourcier@epita.fr>
2022-10-19 16:00:47 +0200

    Update svlete system

---
+++ frontend/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  server: {
+    port: 3000,
+    host: true,
+    strictPort: true,
+    hmr: {
+      clientPort: 3000,
+    },
+  },
+});
