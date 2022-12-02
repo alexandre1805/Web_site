f8b779cb28f838d58e38424f2e382e4f27ded378
alexandre1805 <alexandre.bourcier@epita.fr>
2022-12-02 19:45:22 +0100

    Home page done

--- frontend/src/lib/Components/Landing/Home.svelte
+++ frontend/src/lib/Components/Landing/Home.svelte
@@ -3,4 +3,8 @@
 </script>
 
 <Slider />
-<div class="Home">Welcome to our website !</div>
+<div class="flex h-2/6 justify-between items-center p-20">
+  <div class="font-bold text-xl flex justify-center flex-col"><div class="text-blue-500 text-4xl mx-auto">+ 3</div>games available</div>
+  <div class="font-bold text-xl flex justify-center flex-col"><div class="text-blue-500 text-4xl mx-auto">+ 500</div>users already registered</div>
+  <div class="font-bold text-xl flex justify-center flex-col"><div class="text-blue-500 text-4xl mx-auto">Many</div>More coming</div>
+</div>
