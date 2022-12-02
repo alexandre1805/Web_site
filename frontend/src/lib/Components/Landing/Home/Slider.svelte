c9f02aa8a22341f6b9144fb0e670aa1eea98949a
alexandre1805 <alexandre.bourcier@epita.fr>
2022-12-02 19:33:04 +0100

    Responsive slider done

--- frontend/src/lib/Components/Landing/Home/Slider.svelte
+++ frontend/src/lib/Components/Landing/Home/Slider.svelte
@@ -1,46 +1,72 @@
 <script lang="ts">
-  import slider1 from '../../../../assets/slider/tchat.png'
-  import slider2 from '../../../../assets/slider/connect4.png'
-  import slider3 from '../../../../assets/slider/ticTacToe.png'
+  import tchatImage from '../../../../assets/slider/tchat.png'
+  import Connect4Image from '../../../../assets/slider/connect4.png'
+  import TicTacToeImage from '../../../../assets/slider/ticTacToe.png'
   import leftArrow from '../../../../assets/icons-pack/caret-back-circle-outline.svg'
   import rightArrow from '../../../../assets/icons-pack/caret-forward-circle-outline.svg'
+  import { onMount } from 'svelte'
+  import { text } from 'svelte/internal'
 
-  let current_image : number = 1
+  let slider = {
+    1: {
+      image: tchatImage,
+      text: 'Easy to chat and play !',
+    },
+    2: {
+      image: Connect4Image,
+      text: 'New Connect 4 !',
+    },
+    3: {
+      image: TicTacToeImage,
+      text: "Let's play Tic-Tac-Toe !",
+    },
+  }
+  let current_image: number = 1
 
-  function changeImage(numero: number) {
-    for (let index : number = 0; index < 3; index++) {
-      document.getElementById('slider-' + index).classList.add('invisible')
-    }
-    current_image = current_image > 3 ? 1 : current_image++
-    document.getElementById('slider-' + numero).classList.remove('invisible')
+  onMount(() => {
+    window.setInterval(() => {
+      changeImage(true)
+    }, 10000)
+  })
+
+  function changeImage(clockwise: boolean) {
+    if (clockwise) current_image = current_image >= 3 ? 1 : current_image + 1
+    else current_image = current_image <= 1 ? 3 : current_image - 1
   }
 </script>
 
-<div class="h-4/6 relative">
+<div
+  class="h-4/6 relative bg-blue-600 flex items-center flex-col md:flex-row justify-center md:justify-start"
+>
   <img
-    src={slider1}
-    id="slider-1"
-    alt="slider 1"
-    class="object-cover h-full w-full absolute top-0 left-0"
+    src={slider[current_image].image}
+    alt={slider[current_image].text}
+    class="h-[60%] md:h-[90%] md:ml-[8%]"
   />
-  <img
-    src={slider2}
-    alt="slider 2"
-    class="object-cover h-full w-full absolute top-0 left-0 invisible"
-  />
-  <img
-    src={slider3}
-    alt="slider 3"
-    class="object-cover h-full w-full absolute top-0 left-0 invisible"
-  />
-  <img
-    src={leftArrow}
-    alt="left arrow"
-    class="h-14 absolute top-[50%] left-4"
-  />
-  <img
-    src={rightArrow}
-    alt="left arrow"
-    class="h-14 absolute top-[50%] right-4"
-  />
+  <h2 class="text-2xl md:text-6xl font-bold text-white">
+    {slider[current_image].text}
+  </h2>
+
+  <button
+    on:click={() => {
+      changeImage(false)
+    }}
+  >
+    <img
+      src={leftArrow}
+      alt="left arrow"
+      class="h-14 absolute top-[50%] left-4"
+    />
+  </button>
+  <button
+    on:click={() => {
+      changeImage(true)
+    }}
+  >
+    <img
+      src={rightArrow}
+      alt="left arrow"
+      class="h-14 absolute top-[50%] right-4"
+    />
+  </button>
 </div>
