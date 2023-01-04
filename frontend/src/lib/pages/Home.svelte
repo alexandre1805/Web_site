63757aa3e3c88d32f62922d82a1b41c03558c0d2
alexandre1805 <alexandre.bourcier@epita.fr>
2023-01-04 21:06:04 +0100

    minor changes

--- frontend/src/lib/pages/Home.svelte
+++ frontend/src/lib/pages/Home.svelte
@@ -1,15 +1,24 @@
-<script></script>
+<script>
+    import { link } from 'svelte-spa-router'
+</script>
 
-<div class="block items-center bg-primary">
-    <div class="hero h-screen w-full">
+<div class="block items-center">
+    <div class="hero h-screen w-full bg-home">
         <div class="hero-content flex-col lg:flex-row-reverse">
-          <img src="https://placeimg.com/260/400/arch" class="rounded-lg shadow-2xl" alt="animation"/>
+          <img src="https://placeimg.com/260/400/arch" class="rounded-lg shadow-2xl w-48 md:w-96" alt="animation"/>
           <div>
-            <h1 class="text-6xl font-bold">New Game Website!</h1>
-            <p class="py-6">Register here for free and join us to play many games</p>
-            <button class="btn btn-secondary">Get Started</button>
+            <h1 class="text-8xl font-bold">New Game Website!</h1>
+            <p class="py-6 text-xl">Register here for free and join us to play many games</p>
+            <a class="btn btn-secondary" use:link href="/register">Get Started</a>
           </div>
         </div>
       </div>
-      <div class="h-screen w-screen bg-base-100"></div>
+        <div class="hero h-screen bg-base-200">
+          <div class="hero-content text-center">
+            <div class="max-w-md">
+              <h1 class="text-5xl font-bold">Hello there</h1>
+              <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            </div>
+          </div>
+        </div>
 </div>
